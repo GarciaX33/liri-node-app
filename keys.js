10d8b23b460e50847f23a40a6a465c5ce843b7f9
GarciaX33 <danny.garcia80@hotmail.com>
2019-07-01 14:14:55 -0500

    will edit package json file and keys js file

--- keys.js
+++ keys.js
@@ -1,5 +1,8 @@
 console.log('this is loaded');
 
+SPOTIFY_ID = "9b9ca211d6254779bdf306d184656940";
+SPOTIFY_SECRET = "94257ced851a495c98c66cd850918f51";
+
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
